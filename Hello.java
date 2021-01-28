8475dc7cfec4e6e755373161181822eda175e3e3
rizaman007 <45226574+rizaman007@users.noreply.github.com>
2021-01-28 12:52:48 +0700

    Update Hello.java
    Menambah Input

--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 class Hello {
 	public static void main(String[] args) {
 		System.out.println("************");
 		System.out.println("Hello World");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Masukan Banyak Data = ");
+        	int a = input.nextInt(); 
+		System.out.println("Banyak: " + a);
 	}
 }
